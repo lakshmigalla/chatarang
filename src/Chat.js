9f607c69cd9aaffd31c5547a95b963ddd3fe3889
Lakshmi Galla <lgalla@purdue.edu>
2018-06-12 15:14:51 -0400

    Added MessageList component

--- src/Chat.js
+++ src/Chat.js
@@ -5,11 +5,29 @@ import MessageList from './MessageList';
 import MessageForm from './MessageForm';
 
 class Chat extends Component {
+    constructor () {
+        super()
+
+        this.state = {
+            messages: [
+                {
+                    id: 1,
+                    userName: 'shmi',
+                    body: 'Coolio!'
+                },
+                {
+                    id: 2,
+                    userName: 'kiki',
+                    body: 'Wow. So cool!'
+                }
+            ]
+        }
+    }
     render () {
         return (
             <div className = "Chat">
                 <ChatHeader />
-                <MessageList />
+                <MessageList messages = {this.state.messages}/>
                 <MessageForm />
             </div>
         )
