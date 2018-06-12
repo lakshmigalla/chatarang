8165536344d8ae633d775ded9bc8f9f5932eb153
Lakshmi Galla <lgalla@purdue.edu>
2018-06-12 14:07:35 -0400

    Added sidebar and chat components

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import Sidebar from './Sidebar'
+import Chat from './Chat'
+
+class Main extends Component {
+    render () {
+        return (
+            <div className = "Main">
+                <Sidebar />
+                <Chat />
+            </div>
+        )
+    }
+}
+
+export default Main
